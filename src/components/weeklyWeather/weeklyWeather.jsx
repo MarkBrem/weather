68eabbfe2e95c0064b353ce9a44a894da5c95aae
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2025-06-23 19:58:05 +0300

    .

--- src/components/weeklyWeather/weeklyWeather.jsx
+++ src/components/weeklyWeather/weeklyWeather.jsx
@@ -1,62 +1,49 @@
-import React, {useEffect, useState} from "react";
-import {  Wrapper } from "./weeklyWeather.styled";
-import { fetchWeather } from "components/API/API";
-
-
- 
-
-
+import React, { useEffect, useState } from 'react';
+import { Wrapper, Btn, Tittle, List } from './weeklyWeather.styled';
+import { fetchWeather } from 'components/API/API';
 
 export const WeatherButton = () => {
   const [forecast, setForecast] = useState(null);
 
-useEffect(()=>{
-  fetchWeather('Kyiv')
-  .then((result)=>{
-      setForecast(result); 
-  })
-})
-  
- const getDate = (dt) => {
-    return new Date(dt * 1000).toLocaleDateString("en-US", {
-      weekday: "short",
-      month: "short",
-      day: "numeric",
+  useEffect(() => {
+    fetchWeather('Kyiv').then(result => {
+      setForecast(result);
+    });
+  });
+
+  const getDate = dt => {
+    return new Date(dt * 1000).toLocaleDateString('en-US', {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
     });
   };
 
-
   return (
-    <Wrapper  >
-      <button onClick={fetchWeather}>Show 7-day Forecast</button>
-          <h2 >8-day forecast</h2>
-          <ul >
-            {forecast.map((day, index) => (
-              <li
-                key={index}
-          
-              >
-                <span >{getDate(day.dt)}</span>
+    <Wrapper>
+      <Btn onClick={fetchWeather}>Show 7-day Forecast</Btn>
+      <Tittle>8-day forecast</Tittle>
+      <List>
+        {forecast &&
+          forecast.map((day, index) => (
+            <li key={index}>
+              <span>{getDate(day.dt)}</span>
 
-                <img
-                  src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+              <img
+                src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+                alt="icon"
+              />
 
-          
-                />
+              <span>
+                {Math.round(day.temp.max)}° / {Math.round(day.temp.min)}°C
+              </span>
 
-                <span >
-                  {Math.round(day.temp.max)}° / {Math.round(day.temp.min)}°C
-                </span>
-
-                <span >{day.weather[0].description}</span>
-              </li>
-            ))}
-          </ul>
-      
+              <span>{day.weather[0].description}</span>
+            </li>
+          ))}
+      </List>
     </Wrapper>
-    
   );
 };
-
 
 export default WeatherButton;
