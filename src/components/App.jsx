4f1d3be0c43557e69cf96c1109b8a3d4ead454dd
Марко <mgusanica@gmail.com>
2025-06-27 20:58:44 +0300

    sd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
-import { Container } from "./container/Container";
-import { WeatherInput } from "./hero/hero";
+import { Container } from './container/Container';
+import { WeatherInput } from './hero/hero';
 
 export const App = () => {
-  return <>
-  <Container children={<WeatherInput/>}/>
-  </>
+  return (
+    <>
+      <WeatherInput />
+    </>
+  );
 };
