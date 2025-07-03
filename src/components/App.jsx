1c4c1d69aede04ce86458e378cedb192dc13929c
Марко <mgusanica@gmail.com>
2025-07-03 19:20:37 +0300

    аан

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Container } from './container/Container';
+import { Footer } from './footer/footer';
 import { WeatherInput } from './hero/hero';
 
 export const App = () => {
   return (
     <>
       <WeatherInput />
+      <Footer/>
     </>
   );
 };
