9e39aa284ab5c57e20db62a0a0913e91553581f7
Марко <mgusanica@gmail.com>
2025-07-04 19:51:42 +0300

    віф

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// import { Container } from './container/Container';
 import { WeatherContainer } from './card/WheaterContainer';
 import { Footer } from './footer/footer';
 import { WeatherInput } from './hero/hero';
