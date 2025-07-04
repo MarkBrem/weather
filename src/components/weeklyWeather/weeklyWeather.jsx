68472bff61daddda4a68b530186fc2813430c5ef
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2025-07-04 20:01:02 +0300

    .

--- src/components/weeklyWeather/weeklyWeather.jsx
+++ src/components/weeklyWeather/weeklyWeather.jsx
@@ -3,11 +3,8 @@ import { Wrapper, Btn, Tittle, List } from './weeklyWeather.styled';
 import { fetchWeather } from 'components/API/API';
 
 export const WeatherButton = () => {
-
   const [forecast, setForecast] = useState(null);
 
-
-<<<<<<< Updated upstream
   useEffect(() => {
     fetchWeather('Kyiv').then(result => {
       setForecast(result);
@@ -22,32 +19,8 @@ export const WeatherButton = () => {
     });
   };
 
+  console.log(forecast);
 
-=======
->>>>>>> Stashed changes
-
-  
-useEffect(()=>{
-  fetchWeather('Kyiv')
-  .then((result)=>{
-      setForecast(result); 
-  })
-})
-  
-<<<<<<< Updated upstream
-console.log(forecast);
-
-=======
- const getDate = (dt) => {
-    return new Date(dt * 1000).toLocaleDateString("en-US", {
-      weekday: "short",
-      month: "short",
-      day: "numeric",
-    });
-  };
-
-console.log(forecast);
->>>>>>> Stashed changes
   return (
     <Wrapper>
       <Btn onClick={fetchWeather}>Show 7-day Forecast</Btn>
