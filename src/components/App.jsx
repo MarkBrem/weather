81a3bf47753e0d745719fb002bfd3e3d9a6ab417
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2025-07-07 20:41:28 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,34 @@ import {WeatherButton} from "./weeklyWeather/weeklyWeather"
 import { WeatherContainer } from './card/WheaterContainer';
 import { Footer } from './footer/footer';
 import { WeatherInput } from './hero/hero';
-import { fetchWeeklyWeather } from "./API/API";
 import { Container } from "./container/Container";
 
-
 export const App = () => {
+  const [coord, setCoord] = useState(null)
+  const [showDetail, setDetail] = useState(false);
+  const [showHourlyForecast, setHourlyForecast] = useState(false)
+
+  const handleShowDetail = ()=>{
+    setDetail(prevState => !prevState)
+  }
+
+  const handleShowHourlyForecast = ()=>{
+    setHourlyForecast(prevState => !prevState)
+  }
+
+  const changeCoord = (obj)=>{
+    setCoord(obj)
+  }
+
   return (
     <>
   <Container/>
-  <WeatherButton/>
+  {/* <WeatherButton/> */}
       <WeatherInput />
-      <WeatherContainer/>
+      <WeatherContainer changeCoord={changeCoord} handleShowDetail={handleShowDetail} handleShowHourlyForecast={handleShowHourlyForecast}/>
+      {showDetail && <DetailedInformation coord={coord}/>}
+      {showHourlyForecast && <HourlyForecastChart coord={coord}/>}
+
       <Footer/>
     </>
   );
