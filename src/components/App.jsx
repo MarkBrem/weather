f5e859bc6c4ebd34b38092de253fafd653660516
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2025-07-07 20:10:43 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,43 @@
-
-import { Container } from "./container/Container";
+import { useState } from "react"
+import { DetailedInformation } from './detalInfo/detalInform';
+import { HourlyForecastChart } from './detalInfo/HourlyForecast';
 import {WeatherButton} from "./weeklyWeather/weeklyWeather"
 import { WeatherContainer } from './card/WheaterContainer';
 import { Footer } from './footer/footer';
 import { WeatherInput } from './hero/hero';
+import { fetchWeeklyWeather } from "./API/API";
+import { Container } from "./container/Container";
+
 
 export const App = () => {
+  const [coord, setCoord] = useState(null)
+  const [showDetail, setDetail] = useState(false);
+  const [showHourlyForecast, setHourlyForecast] = useState(false)
 
-
-const handleShowWeather = ()=>{
+  const handleShowDetail = ()=>{
     setDetail(prevState => !prevState)
   }
+
+  const handleShowWeeklyWeather = ()=>{
+    setDetail(prevState => !prevState)
+  }
+  const handleShowHourlyForecast = ()=>{
+    setHourlyForecast(prevState => !prevState)
+  }
+
+  const changeCoord = (obj)=>{
+    setCoord(obj)
+  }
+
   return (
     <>
   <Container/>
   <WeatherButton/>
       <WeatherInput />
-      <WeatherContainer/>
+      <WeatherContainer changeCoord={changeCoord} handleShowDetail={handleShowDetail} handleShowHourlyForecast={handleShowHourlyForecast} handleShowWeeklyWeather={handleShowWeeklyWeather}/>
+      {showDetail && <DetailedInformation coord={coord}/>}
+      {showHourlyForecast && <HourlyForecastChart coord={coord}/>}
+
       <Footer/>
     </>
   );
