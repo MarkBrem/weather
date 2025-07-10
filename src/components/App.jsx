c569bcb4373d95380a512e605b8df73a84340719
Denis <zdenik45@gmail.com>
2025-07-10 20:32:35 +0300

    last try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,44 @@
-import { useState } from "react"
+import { useState } from "react";
 import { DetailedInformation } from './detalInfo/detalInform';
 import { HourlyForecastChart } from './detalInfo/HourlyForecast';
-import {WeatherButton} from "./weeklyWeather/weeklyWeather"
+import { WeatherButton } from "./weeklyWeather/weeklyWeather";
 import { WeatherContainer } from './card/WheaterContainer';
 import { Footer } from './footer/footer';
 import { WeatherInput } from './hero/hero';
 import { Container } from './container/Container';
-import NewsSection from "./container/NewsSection"
+import NewsSection from "./container/NewsSection";
 
 export const App = () => {
-  const [coord, setCoord] = useState(null)
+  const [coord, setCoord] = useState(null);
   const [showDetail, setDetail] = useState(false);
-  const [showHourlyForecast, setHourlyForecast] = useState(false)
+  const [showHourlyForecast, setHourlyForecast] = useState(false);
 
-  const handleShowDetail = ()=>{
-    setDetail(prevState => !prevState)
-  }
+  const handleShowDetail = () => {
+    setDetail(prevState => !prevState);
+  };
 
-  const handleShowHourlyForecast = ()=>{
-    setHourlyForecast(prevState => !prevState)
-  }
+  const handleShowHourlyForecast = () => {
+    setHourlyForecast(prevState => !prevState);
+  };
 
-  const changeCoord = (obj)=>{
-    setCoord(obj)
-  }
+  const changeCoord = (obj) => {
+    setCoord(obj);
+  };
 
-  function App() {
   return (
     <>
       <WeatherInput />
       <WeatherButton />
-      <WeatherContainer />
+      <WeatherContainer
+        changeCoord={changeCoord}
+        handleShowDetail={handleShowDetail}
+        handleShowHourlyForecast={handleShowHourlyForecast}
+      />
       <Container />
-      <DetailedInformation />
-      <HourlyForecastChart />
+      {showDetail && <DetailedInformation coord={coord} />}
+      {showHourlyForecast && <HourlyForecastChart coord={coord} />}
       <NewsSection />
       <Footer />
     </>
   );
-}
-
-  return (
-    <>
-  <Container/>
-  <WeatherButton/>
-      <WeatherInput />
-      <WeatherContainer changeCoord={changeCoord} handleShowDetail={handleShowDetail} handleShowHourlyForecast={handleShowHourlyForecast}/>
-      {showDetail && <DetailedInformation coord={coord}/>}
-      {showHourlyForecast && <HourlyForecastChart coord={coord}/>}
-
-      <Footer/>
-    </>
-  );
-
 };
