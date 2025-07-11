b58dd4bb932bce17909833c1e2d0926b5b75af4f
Марко <mgusanica@gmail.com>
2025-07-11 09:37:55 +0300

    пв

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,7 +30,7 @@ export const App = () => {
   return (
     <>
       <WeatherInput />
-      <WeatherButton />
+      <WeeklyWeather />
       <WeatherContainer
        
         changeCoord={changeCoord}
